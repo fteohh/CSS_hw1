5d5c39129420f60a38a7c8bed7b2a5a8b2c1abc2
fteohh <67304099+fteohh@users.noreply.github.com>
2021-01-05 10:05:10 -0600

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+document.getElementById("w-drum").addEventListener("click", function() {
+    playAudio("sounds/tom-1.mp3");
+});
+document.getElementById("a-drum").addEventListener("click", function() {
+    playAudio("sounds/tom-2.mp3");
+});
+document.getElementById("s-drum").addEventListener("click", function() {
+    playAudio("sounds/tom-3.mp3");
+});
+document.getElementById("d-drum").addEventListener("click", function() {
+    playAudio("sounds/tom-4.mp3");
+});
+document.getElementById("j-drum").addEventListener("click", function() {
+    playAudio("sounds/snare.mp3");
+});
+document.getElementById("k-drum").addEventListener("click", function() {
+    playAudio("sounds/crash.mp3");
+});
+document.getElementById("l-drum").addEventListener("click", function() {
+    playAudio("sounds/kick-bass.mp3");
+});
+
 
 function playAudio(sound){
     var audio = new Audio(sound);
-    audio.play()
+    audio.play();
 }
